befc6f4b01fe7a2ce516ae00a9477805eb83bf89
Matheus Evangelista <matheusevangelistabr@gmail.com>
2020-03-07 00:11:46 -0300

    Criação do front-end da tela de cadastro.

---
+++ app/src/main/java/br/projeto/agenda_dja_tp3/MainActivity.kt
@@ -0,0 +1,17 @@
+package br.projeto.agenda_dja_tp3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+
+class MainActivity : AppCompatActivity(), View.OnClickListener{
+    override fun onClick(v: View?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
